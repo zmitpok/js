2d2a04c0e6c2bc984e366c31fe7ed86b714e9163
vladislav.kuncevich <kuncevich.vladislav@gmail.com>
2023-02-07 20:01:03 +0300

    fcc_4

--- fcc_4/script.js
+++ fcc_4/script.js
@@ -1,5 +1,5 @@
-//Write Reusable JavaScript with Functions
-function reusableFunction() {
-    console.log("Hi World");
-  }
-  reusableFunction()
+//Passing Values to Functions with Arguments
+function functionWithArgs(param1, param2) {
+  console.log(param1 + param2);
+}
+functionWithArgs(3, 4);
