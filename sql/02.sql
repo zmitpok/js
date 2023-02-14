697153f2176ad860381141d0a9c36d33f34bb471
vladislav.kuncevich <kuncevich.vladislav@gmail.com>
2023-02-14 11:30:53 +0300

    sql02

---
+++ sql/02.sql
@@ -0,0 +1,9 @@
+-- Create your SELECT statement here
+SELECT 
+  p.id, 
+  p.name, 
+  COUNT(t.name) AS toy_count
+FROM people AS p
+LEFT JOIN toys AS t
+  ON p.id = t.people_id
+GROUP BY p.id, p.name;
