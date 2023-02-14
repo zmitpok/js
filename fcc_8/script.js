67a37ba1bc3ce99f8c23dc1c1313b9d9e405334b
vladislav.kuncevich <kuncevich.vladislav@gmail.com>
2023-02-14 09:22:28 +0300

    fcc_8

---
+++ fcc_8/script.js
@@ -0,0 +1,13 @@
+//Global vs. Local Scope in Functions
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+const outerWear = 'sweater';
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
