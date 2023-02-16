0151a09dfb4ca6a33914544e74f86b99db53037c
vladislav.kuncevich <kuncevich.vladislav@gmail.com>
2023-02-16 10:30:42 +0300

    fcc_23

---
+++ fcc_all/23.js
@@ -0,0 +1,13 @@
+//Comparison with the Less Than Or Equal To Operator
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val<=50 && val>=25) {
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
